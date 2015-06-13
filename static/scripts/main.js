9ddc28cd00ae72e6cdd1135286342654f9693a5b
TakesxiSximada <takesxi.sximada@gmail.com>
2015-06-14 03:03:39 +0900

    jsのincludeを追加

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,10 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    navigator.splashscreen.hide();
+    if (navigator.splashscreen){
+        navigator.splashscreen.hide();
+    }
+
 }
 
 (function($, doc) {
@@ -12,7 +15,7 @@ function onDeviceReady() {
     	_private,
     	_appData = new AppData(),
     	_isOnline = true;
-    
+
 	//Private methods
 	_private = {
 		getLocation: function(options) {
@@ -24,17 +27,17 @@ function onDeviceReady() {
 			}
 
 			navigator.geolocation.getCurrentPosition(
-				function(position) { 
+				function(position) {
 					dfd.resolve(position);
-				}, 
+				},
 				function(error) {
 					dfd.reject(error);
-				}, 
+				},
 				options);
 
 			return dfd.promise();
 		},
-		
+
 		initMap: function(position) {
 			//Delcare function variables
 			var myOptions,
@@ -45,10 +48,10 @@ function onDeviceReady() {
                 latlng;
 
 			_mapElem = mapElem; //Cache DOM element
-                
+
 			// Use Google API to get the location data for the current coordinates
 			latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-				
+
 			myOptions = {
 				zoom: 11,
 				center: latlng,
@@ -56,10 +59,10 @@ function onDeviceReady() {
 				navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
-			    
+
 			mapObj = new google.maps.Map(mapElem, myOptions);
 			_mapObj = mapObj; //Cache at app level
-			    
+
 			pin = [
 				{
 					position: latlng,
@@ -68,7 +71,7 @@ function onDeviceReady() {
 			];
 
 			_private.addMarkers(pin, mapObj);
-			
+
 			// Get stores nearby
 			_appData.getStarbucksLocations(position.coords.latitude, position.coords.longitude)
 			.done(function(result) {
@@ -78,7 +81,7 @@ function onDeviceReady() {
                                     new google.maps.Size(49, 49),
                                     new google.maps.Point(0, 202));
 
-                
+
 				for (var i = 0; i < len; i++) {
 					locations.push({
 						title: result[i].title + ", " + result[i].description,
@@ -87,19 +90,19 @@ function onDeviceReady() {
 						animation: google.maps.Animation.DROP
 					});
 				}
-                
+
                  _private.addMarkers(locations, mapObj);
 			})
 			.fail(function(e, r, t) {
                 alert("Error loading locations.");
 			});
 		},
-        
+
 		addMarkers: function(locations, mapObj) {
 			var marker,
 			    currentMarkerIndex = 0;
-            
-            
+
+
             function createMarker(index) {
                 if (index < locations.length) {
 					var tmpLocation = locations[index];
@@ -115,31 +118,31 @@ function onDeviceReady() {
 					oneMarkerAtTime();
 				}
 			}
-            
+
 			function oneMarkerAtTime() {
 				google.maps.event.addListener(marker, "animation_changed", function() {
                     if (marker.getAnimation() === null) {
                         createMarker(currentMarkerIndex+=1);
 					}
 				});
-			}				
-            
+			}
+
             createMarker(0);
 		},
-        
+
 		initStoreList: function(position) {
 			_appData.getStarbucksLocations(position.coords.latitude, position.coords.longitude)
         			.done(function(data) {
 						storesListViewModel.load(data);
         			})
         			.fail(function(e, r, t) {
-                        alert("Loading error");    
+                        alert("Loading error");
                     });
 		},
-        
+
 		toggleStoreView: function(index) {
 			var isMap = (index === 0);
-            
+
 			if (isMap) {
 				$(_storeListElem).hide();
 				$(_mapElem).show();
@@ -149,54 +152,54 @@ function onDeviceReady() {
 			}
 		}
 	};
-    
+
 	_app = {
 		init: function() {
 			announcementViewModel.load(_appData.getAnnouncements());
-            
+
             if (window.localStorage.getItem("cards") === null) {
 				localStorage.setItem("cards", _appData.getInitialCards());
 			}
-            
+
             cardsViewModel.loadFromLocalStorage();
 		},
-        
+
 		onAddCardViewShow: function () {
             addCardViewModel.resetView();
 		},
-        
+
 		rewardCardShow: function(e) {
 			var bonusPoints = e.view.params.bonusPoints,
 			    cardNumber = e.view.params.cardNumber;
-            
+
 			rewardsViewModel.setValues(cardNumber, bonusPoints);
-            
+
 		},
-        
+
 		rewardCardInit: function(e) {
 			var container = e.view.content,
     			$cardFront = container.find("#rewardCardFront"),
     			$cardBack = container.find("#rewardCardBack");
-            
+
 			singleCardViewModel.appendCardFadeEffect($cardFront, $cardBack);
 		},
-        
+
 		singleCardShow: function (args) {
 			var cardNumber = args.view.params.cardNumber,
     			bonusPoints = args.view.params.bonusPoints,
     			cardAmount = args.view.params.cardAmount;
-            
+
 			singleCardViewModel.setValues(cardNumber, bonusPoints, cardAmount);
 		},
-        
+
 		singleCardInit: function(e) {
 			var container = e.view.content,
     			$cardFront = container.find("#cardFront"),
     			$cardBack = container.find("#cardBack");
-            
+
 			singleCardViewModel.appendCardFadeEffect($cardFront, $cardBack);
 		},
-        
+
 		storesInit: function() {
 			_mapElem = document.getElementById("map");
 			_storeListElem = document.getElementById("storeList");
@@ -206,25 +209,25 @@ function onDeviceReady() {
 				_private.toggleStoreView(e.sender.selectedIndex);
 			});
 		},
-        
+
 		storesShow: function() {
 			//Don't attempt to reload map/sb data if offline
 			//console.log("ONLINE", _isOnline);
-			if (_isOnline === false) {				
+			if (_isOnline === false) {
 				alert("Please reconnect to the Internet to load locations.");
-    
+
 				return;
 			}
-    
-			_private.getLocation() 
-			.done(function(position) { 
+
+			_private.getLocation()
+			.done(function(position) {
 				_private.initStoreList(position);
-				_private.initMap(position); 
+				_private.initMap(position);
 			})
-			.fail(function(error) { 
-				alert(error.message); /*TODO: Better handling*/ 
+			.fail(function(error) {
+				alert(error.message); /*TODO: Better handling*/
 			});
-            
+
 			if (_isOnline === true) {
 				$("#storesContent").show();
 				$("#offline").hide();
@@ -236,9 +239,9 @@ function onDeviceReady() {
 			}
 		}
 	};
-    
+
 	_app.init();
-        
+
 	$.extend(window, {
 		cardsViewModel: _app.cardsViewModel,
 		rewardCardShow: _app.rewardCardShow,
