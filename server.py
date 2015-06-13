00b3c8d3f6016dd2cea22b71b395bcc42c5a253a
TakesxiSximada <takesxi.sximada@gmail.com>
2015-06-14 01:08:19 +0900

    serverside work

--- server.py
+++ server.py
@@ -1,18 +1,56 @@
 # -*- coding: utf-8 -*-
 import os
-import tornado.ioloop
+import sys
+import braintree
 import tornado.web
-PORT = int(os.getenv('VCAP_APP_PORT', 8000))
+import tornado.ioloop
+
+merchant = os.getenv('merchant', '')
+public_key = os.getenv('public_key', '')
+private_key = os.getenv('private_key', '')
 
 
-class MainHandler(tornado.web.RequestHandler):
+braintree.Configuration.configure(
+    braintree.Environment.Sandbox,
+    merchant,
+    public_key,
+    private_key,
+    )
+
+
+class PingPongHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("PONG")
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+    def post(self):
+        self.write("PONG")
+
+
+class TokenHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('SUCCESS!!')
+
+
+class PaymentHandler(tornado.web.RequestHandler):
+    def get(self):
+        total_amount = str(3)
+        tax_amount = str(1)
+        token = braintree.ClientToken.generate({'customer_id': ''})
+        txn = braintree.Transaction(braintree.Environment.Sandbox, attributes={'token': token, 'amount': total_amount, 'tax_amount': tax_amount})
+        txn.sale({'amount': total_amount, 'payment_method_nonce': 'fake-paypal-future-nonce'})
+        self.write('OK: {}'.format(total_amount))
+
+
+def main():
+    port = int(os.getenv('VCAP_APP_PORT', 8000))
+
+    application = tornado.web.Application([
+        (r"/ping", PingPongHandler),
+        (r"/payment/token", TokenHandler),
+        (r"/payment/buy", PaymentHandler),
+    ])
+    application.listen(port)
+    tornado.ioloop.IOLoop.current().start()
 
 if __name__ == "__main__":
-    application.listen(PORT)
-    tornado.ioloop.IOLoop.current().start()
+    sys.exit(main())
